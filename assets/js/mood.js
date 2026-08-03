61c79f663d81d452510f481ce7ca22c18ed2ae1e
LayanMDamrah <2layanmajed@gmail.com>
2026-08-03 19:16:58 +0300

    dark mood

---
+++ assets/js/mood.js
@@ -0,0 +1,25 @@
+const toggleBtn = document.getElementById("theme-toggle");
+const icon = document.querySelector("#theme-toggle i");
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
+}
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+
+    } else {
+        localStorage.setItem("theme", "light");
+
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+    }
+});
